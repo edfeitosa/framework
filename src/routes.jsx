1dee4de6895b0d24a618833fe33946754e56acad
Eduardo Feitosa <eduardofeitosadg@gmail.com>
2020-03-22 23:14:00 -0300

    posts page

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home/home';
+import Posts from './components/pages/Posts/posts';
 import NotFound from './components/pages/NotFound/notFound';
 
 const Routes = () => {
   return (
     <Switch>
       <Route path='/' exact component={Home} />
+      <Route path='/posts' component={Posts} />
       <Route path='*' component={NotFound} />
     </Switch>
   )
